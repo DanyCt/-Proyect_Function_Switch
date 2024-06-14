133e7629d4eddd45179816bc5500a473bcbe242a
Usuario1 <dcaizatoa.torres@gmail.com>
2024-06-14 10:15:31 -0500

    first commit

---
+++ src/main/java/com/example/proyect_function_switch/HelloApplication.java
@@ -0,0 +1,68 @@
+package com.example.proyect_function_switch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class HelloApplication{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printAnyString("Hi you are the best program, the programs has some options swch option");
+        printAnyString("Please types any option");
+        String readingString = scanner.nextLine();
+        switch (readingString.toLowerCase()) {
+
+            case "a":
+                printAnyString("Row you are in sum program, if is necessary to type tow numbers.");
+                printAnyString("Typing the first number");
+                int numberOne = scanner.nextInt();
+                printAnyString("Typing the first number");
+                int numberTwo = scanner.nextInt();
+                int sumResult = sumNumbers(numberOne, numberTwo);
+                printAnyString("The result of suma is: " + sumResult);
+                break;
+
+            case "b":
+
+                readString();
+                break;
+
+            case "c":
+
+                printAnyString("Typing the first number");
+                int numberfirst = scanner.nextInt();
+                int Results = number(numberfirst);
+                printAnyString("The result of suma is: " + Results);
+                break;
+        }
+
+    }
+
+    public static void printAnyString(String message){
+        System.out.println(message);
+    }
+
+    public static int sumNumbers(int numberOne, int numberTwo) {
+        int result = numberOne+numberTwo;
+        return  result;
+        //return numberOne+numberTwo;
+    }
+
+    public static void readString (){
+        Scanner scanner = new Scanner(System.in);
+        printAnyString("Typing the text");
+        String text = scanner.nextLine();
+        System.out.println(text);
+    }
+
+    public static int number(int numberfirst) {
+
+        return  Math.cos(numberfirst);
+        //return numberOne+numberTwo;
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.proyect_function_switch {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.proyect_function_switch to javafx.fxml;
+    exports com.example.proyect_function_switch;
+}
